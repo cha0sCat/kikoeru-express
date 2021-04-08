fd9bbde4c2dbd30869f5995fae60202266c3b39c
cha0sCat <admin@noob.pw>
2021-04-08 10:59:39 +0800

    feat: sign offload media url

--- routes/utils/sign.js
+++ routes/utils/sign.js
@@ -8,11 +8,12 @@ const { config } = require('../../config');
 // 签名内容为路径 + 秒级时间戳
 // 签名查询键 verify
 const signStaticFileUrl = (rawUrl, timestamp=Date.now() / 1000 | 0) => {
-  const url = new URL(rawUrl)
+  const url = new URL(rawUrl);
   const hmac = crypto.createHmac('sha256', config.signsecret)
       .update(url.pathname)
-      .update(timestamp.toString())
-  url.searchParams.append("verify", encodeURIComponent(hmac.digest("base64")));
+      .update(timestamp.toString());
+  const sign = encodeURIComponent(hmac.digest("base64"));
+  url.searchParams.append("verify", `${timestamp.toString()}-${sign}`);
   return url.href;
 }
 
