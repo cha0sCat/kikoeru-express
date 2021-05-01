14c64e2cc51fcac5ff3f92e9777f36ad743fd866
mason1900 <mw2010a@gmail.com>
2021-05-01 10:43:15 +0800

    chore: add health endpoint

--- api.js
+++ api.js
@@ -18,12 +18,13 @@ const getToken = (req) => {
 
 module.exports = (app) => {
   if (config.auth) {
-    // expressJwt 中间件 
+    // expressJwt 中间件
     // 验证指定 http 请求的 JsonWebTokens 的有效性, 如果有效就将 JsonWebTokens 的值设置到 req.user 里面, 然后路由到相应的 router
     app.use('/api', expressJwt({ secret: config.jwtsecret, getToken, algorithms: ['HS256'] })
         .unless({ path: [
             {url: '/api/auth/me'},
-            {url: '/api/auth/reg', methods: ["GET", "POST"]}
+            {url: '/api/auth/reg', methods: ["GET", "POST"]},
+            {url: '/api/health'}
         ] }));
   }
 
