d5e598051ccccad5142feb0be0339d96e2057a4b
cha0sCat <admin@noob.pw>
2021-05-27 17:00:22 +0800

    add knex environment for production

--- database/knexfile.js
+++ database/knexfile.js
@@ -25,6 +25,28 @@ module.exports = {
     }
   },
 
+  production: {
+    client: 'sqlite3', // 数据库类型
+    useNullAsDefault: true,
+    connection: { // 连接参数
+      filename: path.join(config.databaseFolderDir, 'db.sqlite3'),
+    },
+    acquireConnectionTimeout: 4000, // 连接计时器
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON;', function (err) {
+          if (err) {
+            done(err, conn);
+          } else {
+            conn.run(`PRAGMA busy_timeout = ${config.dbBusyTimeout};`, function (err) {
+              done(err, conn);
+            });
+          }
+        });
+      }
+    }
+  },
+
   // For migration only. Foreign keys are disabled (SQLite default)
   upgrade: {
     client: 'sqlite3',
